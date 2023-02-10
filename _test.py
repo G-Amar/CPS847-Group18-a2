12cccbee00252ba825c17c43f7ed9a9e90cf1714
Amar Gupta <amargupta3327@gmail.com>
2023-02-09 21:40:51 -0500

    add testcases for cube function

--- _test.py
+++ _test.py
@@ -3,7 +3,7 @@ Test helper functions
 """
 
 import unittest
-from func import square
+from func import square, cube
 
 class TestSampleMethods(unittest.TestCase):
     """
@@ -18,6 +18,14 @@ class TestSampleMethods(unittest.TestCase):
         self.assertEqual(square(6), 36)
         self.assertEqual(square(-3), 9)
         self.assertEqual(square(-1), 1)
+        
+    def test_cube(self):
+        """
+        Test increments
+        """
+        self.assertEqual(cube(0), 0)
+        self.assertEqual(cube(1), 1)
+        self.assertEqual(cube(-1), -1)
 
 if __name__ == '__main__':
     unittest.main()
